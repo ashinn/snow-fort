61b43247c3ece18f9b361d14dd1d00e555314f17
Alex Shinn <alexshinn@gmail.com>
2015-04-26 23:46:12 +0900

    Showing maintainers when specified and different from authors.

--- pkg/index.scm
+++ pkg/index.scm
@@ -1,12 +1,16 @@
 
 (import (scheme base) (scheme write) (srfi 1) (srfi 95)
         (chibi log) (chibi net servlet) (chibi config) (chibi pathname)
-        (chibi string) (chibi snow fort) (chibi snow package))
+        (chibi string) (chibi regexp) (chibi snow fort) (chibi snow package))
 
 ;; Add a table id, and a sort button to each header to call a sorter
 ;; function on the table.
 (define (sortable-table table)
   table)
+
+(define (extract-email str)
+  (string-trim
+   (regexp-replace '(: (* any) "<" ($ (* (~ (">")))) ">" (* any)) str 1)))
 
 (define (package-row cfg repo pkg)
   (let* ((email (package-email pkg))
@@ -17,7 +21,15 @@
                            (or (string-prefix? doc "http:")
                                (string-prefix? doc "https:")))
                       doc
-                      (make-path (static-url cfg dir) "index.html"))))
+                      (make-path (static-url cfg dir) "index.html")))
+         (auth (package-author repo pkg))
+         (maint (package-maintainer repo pkg))
+         (auth-email (if (and maint (not (equal? auth maint)))
+                         (cond
+                          ((assoc-get (cdr pkg) 'authors)
+                           => extract-email)
+                          (else #f))
+                         email)))
     `(tr
       (td (a (@ (href . ,(assoc-get pkg 'url)))
              ,(let ((out (open-output-string)))
@@ -25,8 +37,13 @@
                 (get-output-string out))))
       (td ,(package-version pkg))
       (td (@ (class . "detail")) ,desc)
-      (td (a (@ (href . ,(string-append "mailto:" (or email ""))))
-             ,(package-author repo pkg)))
+      (td (a (@ (href . ,(string-append "mailto:" (or auth-email ""))))
+             ,auth)
+          ,@(if (and maint (not (equal? auth maint)))
+                `((br)
+                  "(" (a (@ (href . ,(string-append "mailto:" (or email ""))))
+                         ,maint) ")")
+                '()))
       (td (a (@ (href . ,doc-url)) "[html]")))))
 
 (define (write-to-string x)
@@ -51,7 +68,7 @@
                (sortable-table
                 `(table
                   (tr (th "Package") (th "Version") (th "Description")
-                      (th "Author") (th "Docs"))
+                      (th "Authors") (th "Docs"))
                   ,@(map
                      (lambda (pkg) (package-row cfg repo pkg))
                      (sort (filter package? (cdr repo))
